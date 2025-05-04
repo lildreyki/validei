40049e381a6dd5e61601cee87b44e93334f270e4
lildreyki <lildreyki@icloud.com>
2025-05-04 19:14:43 -0300

    0405251914

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
-// what and now?
+// funciona pelo amor de deus
+
+function loginAcess() {
+    var login = document.getElementById('ilogin').value
+    var password = document.getElementById('isenha').value
+    if (login == "admin" && password == "1234") {
+        alert("Login realizado com sucesso!")
+        window.location.href = "validei.html";
+    } else {
+        alert("Usuário ou senha incorretos!")
+    }
+}
